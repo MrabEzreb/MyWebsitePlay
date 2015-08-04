6eabc67d60e810a09a651602b246bc4a9fbe2c27
MrabEzreb <zerbebram@gmail.com>
2015-08-04 14:24:21 -0400

    Heading home for today :DDDDD EYM SMAYRT :D

--- app/controllers/Games.java
+++ app/controllers/Games.java
@@ -4,11 +4,12 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import play.twirl.api.Html;
 import views.html.basepage2;
+import views.html.content.games.minecraft;
 
 public class Games extends Controller {
 
 	public Result minecraft() {
-		return ok(basepage2.render("Minecraft", Home.NAVBAR, new Html(""), new Html("")));
+		return ok(basepage2.render("Minecraft", Home.NAVBAR, new Html(""), minecraft.render()));
 	}
 	
 }
